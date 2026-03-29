c8be2dff5e71a5c936a21e3d5914e9bdf54e7b38
xoproject0-collab <xoproject0@gmail.com>
2026-03-29 17:54:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from config import TOKEN, VALUE_UPDATE_INTERVAL
-from services.value_service import load_all_pets, get_value, pets
+from services.value_service import load_all_pets
 from handlers.start import start
 from keyboards.menus import main_menu
 
-bot = Bot(TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
 # Планировщик для обновления value
 scheduler = AsyncIOScheduler()
@@ -17,12 +16,14 @@ scheduler.add_job(load_all_pets, "interval", minutes=VALUE_UPDATE_INTERVAL)
 scheduler.start()
 
 # Регистрируем команду /start
-dp.register_message_handler(start, commands=["start"])
+dp.message.register(start, commands=["start"])
 
-async def on_startup(dp):
+async def main():
     print("[INFO] Бот запускается...")
     await load_all_pets()
     print("[INFO] Value питомцев загружено.")
+    # Запуск polling
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup)
+    asyncio.run(main())
