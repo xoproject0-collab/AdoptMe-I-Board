1064c170d72db1fc50613b835cd09c2f4309a91c
xoproject0-collab <xoproject0@gmail.com>
2026-03-29 18:01:21 +0300

    Update menus.py

--- keyboards/menus.py
+++ keyboards/menus.py
@@ -1,1 +1,13 @@
+# keyboards/menus.py
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu():
+    kb = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="➕ Создать трейд")],
+            [KeyboardButton(text="📋 Смотреть трейды")],
+            [KeyboardButton(text="📦 Мои трейды")]
+        ],
+        resize_keyboard=True
+    )
+    return kb
