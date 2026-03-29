46a0f494ac44f42bb17e5acb940574470d054f2f
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 02:38:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,1 @@
-# config.py
-TOKEN = "8585113754:AAGvp2FVXEn5kUIcIvK_rklra8fZlsBYZ5o"  # Токен вашего бота
-VALUE_UPDATE_INTERVAL = 10      # Интервал обновления value (в минутах)
+TOKEN = os.getenv("8585113754:AAGvp2FVXEn5kUIcIvK_rklra8fZlsBYZ5o")
