121fa7ffd77d947505e4da10931a35fb3e131764
xoproject0-collab <xoproject0@gmail.com>
2026-03-29 21:31:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-TOKEN = os.environ.get("TOKEN")  # берем токен из переменных окружения Railway
-VALUE_UPDATE_INTERVAL = 10       # обновление цен в минутах
+TOKEN = os.environ.get("TOKEN")
+VALUE_UPDATE_INTERVAL = 10
