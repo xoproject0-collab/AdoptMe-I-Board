389a14b3888e7089c44782243823719b5a613cc2
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 04:33:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
 from config import TOKEN, UPDATE_INTERVAL
-from handlers import start, trade
+from handlers import start, pets
 from services.value_service import load_all_pets
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,14 +13,13 @@ async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
-    # Подключаем роутеры
     dp.include_router(start.router)
-    dp.include_router(trade.router)
+    dp.include_router(pets.router)
 
-    # Загружаем всех питомцев
+    # Загружаем всех питомцев сразу
     await load_all_pets()
 
-    # Автообновление value каждые UPDATE_INTERVAL секунд
+    # Автообновление value каждые 10 минут
     scheduler = AsyncIOScheduler()
     scheduler.add_job(load_all_pets, "interval", seconds=UPDATE_INTERVAL)
     scheduler.start()
