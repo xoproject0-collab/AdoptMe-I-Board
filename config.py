68722d3728f4b219550018a217bed725116e6497
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 03:52:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 
-import os
-
-TOKEN = os.getenv("8585113754:AAGvp2FVXEn5kUIcIvK_rklra8fZlsBYZ5o")
-UPDATE_INTERVAL = 600  # каждые 10 минут обновлять value
+TOKEN = "8585113754:AAEuFxy-rHCCAvvxOdLcCtKej5g82MvLU1E"
+UPDATE_INTERVAL = 600  # 10 минут
