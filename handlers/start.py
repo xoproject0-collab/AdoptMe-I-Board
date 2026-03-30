25e7de945b75eba9887b4314db6a632d8d6e4852
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 21:39:16 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,4 @@
 # handlers/start.py
-from aiogram import Router
-from aiogram.filters import Command
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-
-router = Router()
-
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     kb = InlineKeyboardMarkup(row_width=2)
