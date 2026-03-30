2d02d32c9cc1687e75be95b1d0baaf05203c54d1
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 22:57:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,26 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import BotCommand
+from aiogram import Bot, Dispatcher
+
 from handlers import start, trade, pets
 
-TOKEN = "8585113754:AAEuFxy-rHCCAvvxOdLcCtKej5g82MvLU1E"
+BOT_TOKEN = "8585113754:AAEuFxy-rHCCAvvxOdLcCtKej5g82MvLU1E"
+
 
 async def main():
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
-    # Подключаем все роутеры (handlers)
     dp.include_router(start.router)
     dp.include_router(trade.router)
     dp.include_router(pets.router)
 
-    # Загружаем питомцев
+    # загрузка питомцев при старте
     await pets.load_all_pets()
 
-    # Настраиваем команды для бота
-    await bot.set_my_commands([
-        BotCommand(command="start", description="Запустить бота"),
-        BotCommand(command="pets", description="Список всех питомцев"),
-        BotCommand(command="trade", description="Анализ трейдов")
-    ])
-
     print("🚀 Бот запущен")
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
+
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
