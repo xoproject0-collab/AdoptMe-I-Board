184dbbf37cc889f8988f842dcbec6a458dd0fe3b
xoproject0-collab <xoproject0@gmail.com>
2026-03-30 19:20:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from handlers import start, trade
 import handlers.pets as pets  # сервис для питомцев
 
-BOT_TOKEN = "8585113754:AAEuFxy-rHCCAvvxOdLcCtKej5g82MvLU1E"
+BOT_TOKEN = "YOUR_TOKEN_HERE"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -15,7 +15,7 @@ dp.include_router(trade.router)
 
 async def main():
     # Загружаем питомцев
-    await pets.load_all_pets()  # <- исправлен отступ
+    await pets.load_all_pets()  # исправлен отступ и вызов
 
     print("🚀 Бот запущен")
     await dp.start_polling(bot)
